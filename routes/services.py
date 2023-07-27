900485e66e261e3f7a067cf60192dafe2add00f8
irv <madmann1986@gmail.com>
2023-07-27 22:59:14 +0300

    add dfs_func, add in find_route func to find right ways

--- routes/services.py
+++ routes/services.py
@@ -1,14 +1,79 @@
 from trains.models import Train
 
 
-def get_graph(qs):
+def dfs_paths(graph, start, end):
+    """функция поиска всех возможных маршрутов
+    из одного города в другой. Вариант посещения
+     одного и тогоже города более одного раза
+     не рассматривается
+     """
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        if vertex in graph.keys():
+            for next_ in graph[vertex] - set(path):
+                if next_ == end:
+                    yield path + [next_]
+                else:
+                    stack.append((next_, path + [next_]))
+
+
+def get_graph(qs) -> dict:
+    """функция создает словарь где key=id городов
+    а value=связи с другими городами"""
     graph = {}
-    for query in qs:
-        graph.setdefault(query.from_city_id, set())
-        graph[query.from_city_id].add(query.to_city_id)
-        return graph
+    for q in qs:
+        graph.setdefault(q.from_city_id, set())
+        graph[q.from_city_id].add(q.to_city_id)
+    return graph
 
 
 def get_routes(request, form) -> dict:
+    """функция поиска маршрутов"""
+    context = {'form': form}
     qs = Train.objects.all()
     graph = get_graph(qs)
+    data = form.cleaned_data
+    from_city = data['from_city']
+    to_city = data['to_city']
+    cities = data['cities']
+    travelling_time = data['travelling_time']
+
+    all_ways = list(dfs_paths(graph, from_city.id, to_city.id))
+    if not len(all_ways):
+        # не ни одного маршрута для данного поиска
+        raise ValueError('Маршрута не существует')
+    if cities:
+        # если есть города, через которые нудно проехать
+        _cities = [city.id for city in cities]
+        right_ways = []
+        for route in all_ways:
+            if all(city in route for city in _cities):
+                right_ways.append(route)
+        if not right_ways:
+            # когда список маршрутов пуст
+            raise ValueError('Маршрут через эти города невозможен')
+    else:
+        right_ways = all_ways
+    # создаем словарь с маршрутами с возможным маршрутами
+    trains = []
+    all_trains = {}
+    for q in qs:
+        all_trains.setdefault((q.from_city_id, q.to_city_id), [])
+        all_trains[(q.from_city_id, q.to_city_id)].append(q)
+
+    for route in right_ways:
+        tmp = {}
+        tmp['trains'] = []
+        total_time = 0
+        for i in range(len(route) - 1):
+            qs = all_trains[(route[i], route[i + 1])]
+            q = qs[0]
+            total_time += q.travel_time
+            tmp['trains'].append(qs)
+        tmp['total_time'] = total_time
+        if total_time <= travelling_time:
+            trains.append(tmp)
+    if not trains:
+        raise ValueError('Время в пути больше заданного')
+    return context
